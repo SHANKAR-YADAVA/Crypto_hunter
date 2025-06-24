15dfac18114bd7375ae059c4e5dff5eb326ca1dd
SHANKAR-YADAVA <shankaryadavar2004@gmail.com>
2025-06-24 11:13:29 +0530

    db changes

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,12 @@
+// src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import "firebase/compat/auth";
-import "firebase/compat/firestore";
-import firebaseConfig from "./config/firebaseConfig";
+import firebaseConfig from "./config/firebaseConfig"; 
 
 const firebaseApp = initializeApp(firebaseConfig);
 
-const auth = getAuth(firebaseApp); // For Authentication
-const db = getFirestore(firebaseApp); // For Using Database
+const auth = getAuth(firebaseApp);       
+const db = getFirestore(firebaseApp);    
 
 export { auth, db };
